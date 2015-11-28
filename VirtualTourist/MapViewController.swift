5c7cf0453e158aa80b2066a438df4c14367171e6
Brian King <brian@liip.ch>
2015-11-28 15:30:53 +0100

    Added MapView

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+	@IBOutlet weak var mapView: MKMapView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
