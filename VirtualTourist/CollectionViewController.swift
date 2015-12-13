ac22821551f048edb0d0b71e3e2f0729eff3669d
Brian King <free.as.in.speech@gmail.com>
2015-12-13 14:09:19 +0100

    Add placeholder image and layout collection view cells

--- VirtualTourist/CollectionViewController.swift
+++ VirtualTourist/CollectionViewController.swift
@@ -11,11 +11,29 @@ import CoreData
 
 class CollectionViewController: UIViewController {
 
+	@IBOutlet weak var collectionView: UICollectionView!
+
 	var pin: Pin!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		collectionView.dataSource = self
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+}
 
+extension CollectionViewController: UICollectionViewDataSource {
+
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 20
+	}
+
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PhotoCell", forIndexPath: indexPath)
+		let imageView = UIImageView(frame: cell.contentView.frame)
+		imageView.contentMode = .ScaleAspectFit
+		imageView.image = UIImage(named: "Placeholder")
+		cell.contentView.addSubview(imageView)
+		return cell
+	}
 }
