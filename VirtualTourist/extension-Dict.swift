ab6b64390e972a7fe5aeded42df84aff0532d2e0
Brian King <brian@liip.ch>
2016-01-09 14:51:10 +0100

    Add comment

--- VirtualTourist/extension-Dict.swift
+++ VirtualTourist/extension-Dict.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+/**
+Redefine dictionary addition to mean add all items from right-hand side dict
+to the left-hand side dict, overwriting any key/value pairs for which the
+key already exists in the left-hand side dict.
+*/
 func +<K, V> (var left: [K : V], right: [K : V]) -> [K: V] {
 	for (k, v) in right {
 		left[k] = v
