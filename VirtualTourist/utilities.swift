226aa1b1c13d1f6ce0531c17775077d8fe2ab9a8
Brian King <brian@liip.ch>
2016-01-09 14:50:55 +0100

    Add presentAlert method

--- VirtualTourist/utilities.swift
+++ VirtualTourist/utilities.swift
@@ -6,8 +6,30 @@
 //  Copyright © 2015 Brian King. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
 
 func async_main(block: () -> Void) {
 	dispatch_async(dispatch_get_main_queue(), block)
+}
+
+
+class Utility {
+
+	func presentAlert(title: String?, message: String?) {
+
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+
+		guard var controller = UIApplication.sharedApplication().keyWindow?.rootViewController else {
+			print("presentAlert: Unable to get rootViewController")
+			return
+		}
+
+		while let presentedController = controller.presentedViewController {
+			controller = presentedController
+		}
+
+		controller.presentViewController(alertController, animated: true, completion: nil)
+	}
 }
