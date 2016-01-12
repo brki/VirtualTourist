b9d03df9fe40e1292d67e9c054ff77774c3350eb
Brian King <free.as.in.speech@gmail.com>
2016-01-12 08:01:45 +0100

    Fix image zooming / panning

--- VirtualTourist/PhotoDetailViewController.swift
+++ VirtualTourist/PhotoDetailViewController.swift
@@ -8,29 +8,73 @@
 
 import UIKit
 
+/**
+A zoomable detail view.
+
+I struggled for quite a while trying to have the image zooming and panning 
+behaving nicely on my own; apparently I'm not the only one to have had
+problems with this, judging from Stackoverflow.
+
+This pure auto-layout solution, which updates constraint constants in the code,
+is based on https://github.com/evgenyneu/ios-imagescroll-swift .  It has beenn
+adapted so that it also plays nicely when showing / hiding the navigation and
+status bars.
+*/
 class PhotoDetailViewController: UIViewController {
 
 	var thumbnailImage: UIImage!
 	var photo: Photo!
+	var lastZoomScale: CGFloat = 0.0
 
 	@IBOutlet var scrollView: UIScrollView!
 	@IBOutlet var imageView: UIImageView!
+	@IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
+	@IBOutlet weak var imageConstraintRight: NSLayoutConstraint!
+	@IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
+	@IBOutlet weak var imageConstraintTop: NSLayoutConstraint!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		scrollView.delegate = self
-		configureZooming()
-		imageView.image = thumbnailImage
+		scrollView.maximumZoomScale = 4.0
 		addDownloadOperation()
+	}
+
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		if imageView.image == nil {
+			imageView.image = thumbnailImage
+		}
+		updateZoom()
 	}
 
 	override func viewWillDisappear(animated: Bool) {
 		navigationController?.navigationBarHidden = false
+		UIApplication.sharedApplication().statusBarHidden = false
 	}
 
-	func configureZooming() {
-		scrollView.minimumZoomScale = 1.0
-		scrollView.maximumZoomScale = 4.0
+	/**
+	Hide / show the navigation and status bar in response to a single tap.
+	*/
+	@IBAction func imageViewTapped(sender: UITapGestureRecognizer) {
+		toggleFullScreen()
+	}
+
+	func toggleFullScreen() {
+		let app = UIApplication.sharedApplication()
+		let navController = navigationController!
+		let hidden = !navController.navigationBarHidden
+		navController.navigationBarHidden = hidden
+		app.statusBarHidden = hidden
+
+//		let verticalAdjustment = (navController.navigationBar.frame.height + app.statusBarFrame.height) / 2
+//		if fullScreen {
+//			scrollViewCenterYToSuperviewCenterY.constant = 0
+//			scrollView.contentSize.height -= verticalAdjustment
+//		} else {
+//			scrollViewCenterYToSuperviewCenterY.constant = -verticalAdjustment
+//			scrollView.contentSize.height += verticalAdjustment
+//		}
 	}
 
 	func addDownloadOperation() {
@@ -49,21 +93,85 @@ class PhotoDetailViewController: UIViewController {
 			}
 			async_main {
 				self.imageView.image = image
+				self.updateZoom()
 			}
 		}
 		operation.queuePriority = .VeryHigh
 		QueueManager.filesDownloadQueue.addOperation(operation)
 	}
-	
-	@IBAction func imageViewTapped(sender: UITapGestureRecognizer) {
-		if let navController = navigationController {
-			navController.navigationBarHidden = !navController.navigationBarHidden
+
+	// MARK: Zoom adjustments
+
+	/**
+	When the view size changes, adjust the zoom, which will trigger an update of the constraints.
+	*/
+	override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+		super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+
+		coordinator.animateAlongsideTransition(
+			{ [weak self] _ in
+				self?.updateZoom()
+			},
+			completion: nil
+		)
+	}
+
+	func updateConstraints() {
+		if let image = imageView.image {
+			let imageWidth = image.size.width
+			let imageHeight = image.size.height
+
+			let viewWidth = scrollView.bounds.size.width
+			let viewHeight = scrollView.bounds.size.height
+
+			// center image if it is smaller than the scroll view
+			var hPadding = (viewWidth - scrollView.zoomScale * imageWidth) / 2
+			if hPadding < 0 {
+				hPadding = 0
+			}
+
+			var vPadding = (viewHeight - scrollView.zoomScale * imageHeight) / 2
+			if vPadding < 0 {
+				vPadding = 0
+			}
+
+			imageConstraintLeft.constant = hPadding
+			imageConstraintRight.constant = hPadding
+
+			imageConstraintTop.constant = vPadding
+			imageConstraintBottom.constant = vPadding
+
+			view.layoutIfNeeded()
+		}
+	}
+
+	// Zoom to show as much image as possible unless image is smaller than the scroll view
+	private func updateZoom() {
+		if let image = imageView.image {
+			var minZoom = min(scrollView.bounds.size.width / image.size.width, scrollView.bounds.size.height / image.size.height)
+
+			if minZoom > 1 { minZoom = 1 }
+
+			scrollView.minimumZoomScale = minZoom
+
+			// Force scrollViewDidZoom fire if zoom did not change
+			if minZoom == lastZoomScale { minZoom += 0.000001 }
+
+			scrollView.zoomScale = minZoom
+			lastZoomScale = minZoom
 		}
 	}
 }
 
+
+// MARK: UIScrollViewDelegate
+
 extension PhotoDetailViewController: UIScrollViewDelegate {
 	func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
 		return imageView
+	}
+
+	func scrollViewDidZoom(scrollView: UIScrollView) {
+		updateConstraints()
 	}
 }
