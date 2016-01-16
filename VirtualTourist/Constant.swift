8024da19ebae1d695e3f6125d6a68f14240d5e27
Brian King <free.as.in.speech@gmail.com>
2016-01-16 23:45:38 +0100

    Adjust and explain the photosPerPage constant

--- VirtualTourist/Constant.swift
+++ VirtualTourist/Constant.swift
@@ -10,7 +10,13 @@ import Foundation
 
 struct Constant {
 	static let maxPhotoPagesPerCollection = 3
-	static let photosPerPage = 3
+
+	// Flickr default per_page value is 250.  Set to a low value so that
+	// the fetch-multiple-pages-of-results logic gets used, and so that there
+	// are not too many photos for each collection view:
+	static let photosPerPage = 5
+
 	static let maxFlickrGeoQueryResults = 4000
+
 	static let documentDir = try! NSFileManager().URLForDirectory(.DocumentDirectory, inDomain: .UserDomainMask, appropriateForURL: nil, create: true)
 }
