ee9894d211a6e489ae75d493101abeb402e2d737
Brian King <free.as.in.speech@gmail.com>
2016-01-16 21:43:09 +0100

    Delay presentation of error alert if front view controller is currently being presented or dismissed

--- VirtualTourist/utilities.swift
+++ VirtualTourist/utilities.swift
@@ -21,14 +21,26 @@ class Utility {
 		let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
 		alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
 
+		presentAlertControllerOnFrontController(alertController)
+	}
+
+	static func presentAlertControllerOnFrontController(alertController: UIAlertController) {
 		async_main {
 			guard var controller = UIApplication.sharedApplication().keyWindow?.rootViewController else {
-				print("presentAlert: Unable to get rootViewController")
+				print("presentAlertControllerOnFrontController: Unable to get rootViewController")
 				return
 			}
 
 			while let presentedController = controller.presentedViewController {
 				controller = presentedController
+			}
+
+			// If view controller is being dismissed or presented, delay presentation a bit.
+			if controller.isBeingDismissed() || controller.isBeingPresented() {
+				dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) {
+					presentAlertControllerOnFrontController(alertController)
+				}
+				return
 			}
 
 			controller.presentViewController(alertController, animated: true, completion: nil)
